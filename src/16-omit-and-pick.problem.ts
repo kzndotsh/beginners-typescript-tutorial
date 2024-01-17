b1a3fe57aae3ec9a24214e8776d90dbdb76032bf
kaizen <admin@kaizen.wtf>
2024-01-16 22:20:03 -0500

    [Finish] 16

--- src/16-omit-and-pick.problem.ts
+++ src/16-omit-and-pick.problem.ts
@@ -11,6 +11,5 @@ interface User {
  * firstName and lastName properties of User?
  */
 
-type MyType = unknown;
-
+type MyType = Omit<User, "id">;
 type tests = [Expect<Equal<MyType, { firstName: string; lastName: string }>>];
